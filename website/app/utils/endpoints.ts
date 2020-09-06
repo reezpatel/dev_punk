0a758ce6b4b51eb136e08b9bda8509dd2137f9cf
Reez <reezpatel@gmail.com>
2020-09-06 18:13:23 +0530

    Fixed Endpoint for image upload

--- website/app/utils/endpoints.ts
+++ website/app/utils/endpoints.ts
@@ -6,7 +6,7 @@ const ENDPOINTS = {
   feedBanner: (id: string): string => `${BASE_URL}/api/v1/images/feeds/${id}`,
   redirect: (id: string): string => `${BASE_URL}/api/v1/r/${id}`,
   authCode: (code: string): string => `${BASE_URL}/api/v1/auth/${code}`,
-  imageUpload: `${BASE_URL}/api/images`,
+  imageUpload: `${BASE_URL}/api/v1/images/website`,
   gqlServer: `${BASE_URL}/graphql`,
   authUrl: `${BASE_URL}/api/v1/auth/validate`
 };
